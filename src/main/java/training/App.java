4c07d06bfdd8b688242e2d294fc03cba7297d647
suresh <vskreddy652@yahoo.com>
2017-07-02 03:25:28 +0530

    Initial commit

---
+++ src/main/java/training/App.java
@@ -0,0 +1,14 @@
+package training;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        System.out.println("This is second line. Can you see it?");
+    }
+}
