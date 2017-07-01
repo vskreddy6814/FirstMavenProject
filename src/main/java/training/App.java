fc5b7c289efdebb6c6e86311b0532de8533e3fd7
vskreddy6814 <vskreddy6814@gmail.com>
2017-07-02 02:52:16 +0530

    Update App.java

--- src/main/java/training/App.java
+++ src/main/java/training/App.java
@@ -9,6 +9,6 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
-        System.out.println("This is second line. Can you see it?");
+        System.out.println("This is second line. Can you see it? This change made form online");
     }
 }
